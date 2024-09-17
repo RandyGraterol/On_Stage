728a4e81042bc1fbe1825e821d97d0b192839fce
RandyGraterol <soapdelinger@gmail.com>
2024-09-17 21:29:14 +0200

    Update memoria.js
    cambion de extensiones

--- utils/memoria.js
+++ utils/memoria.js
@@ -1,20 +1,23 @@
 const conciertos = ['conciertos-01.jpg','conciertos-02.png','conciertos-03.jpg','conciertos-04.jpg','conciertos-05.jpeg','conciertos-06.jpeg','conciertos-07.jpeg','conciertos-08.jpeg','conciertos-09.jpeg','conciertos-10.jpeg','conciertos-11.jpeg','conciertos-12.png','conciertos-13.png','conciertos-14.png','conciertos-15.png','conciertos-16.jpg'];
 
-const estructura = ['estructura-01.jpeg','estructura-02.jpeg','estructura-03.png','estructura-04.png','estructura-05.png','estructura-06.png','estructura-07.png','estructura-08.png','estructura-09.png','estructura-10.png','estructura-11.jpeg','estructura-12.jpeg','estructura-13.jpeg','estructura-14.jpeg'];
+const estructura = ['estructura-01.JPEG','estructura-02.JPEG','estructura-03.PNG','estructura-04.PNG','estructura-05.PNG','estructura-06.PNG','estructura-07.PNG','estructura-08.PNG','estructura-09.PNG','estructura-10.PNG','estructura-11.JPEG','estructura-12.JPEG','estructura-13.JPEG','estructura-14.JPEG'];
 
 const iluminacion = ['iluminacion-01.jpeg','iluminacion-02.png','iluminacion-03.jpeg','iluminacion-04.jpeg','iluminacion-05.jpeg','iluminacion-06.jpeg','iluminacion-07.jpeg','iluminacion-08.jpeg','iluminacion-09.jpeg','iluminacion-10.png','iluminacion-11.png','iluminacion-12.jpeg','iluminacion-13.png','iluminacion-14.jpeg'];
 
-const pantallaLed = ['pantalla-01.png','pantalla-02.png','pantalla-03.png','pantalla-04.png','pantalla-05.jpeg','pantalla-06.jpeg','pantalla-07.jpeg','pantalla-08.jpeg','pantalla-09.jpeg','pantalla-10.jpeg','pantalla-11.jpeg','pantalla-12.jpeg','pantalla-13.png','pantalla-14.jpeg','pantalla-15.jpeg','pantalla-16.jpeg','pantalla-17.png','pantalla-18.jpeg','pantalla-19.jpeg','pantalla-20.png'];
+const pantallaLed = ['pantalla-01.PNG','pantalla-02.PNG','pantalla-03.PNG','pantalla-04.PNG','pantalla-05.JPEG','pantalla-06.JPEG','pantalla-07.JPEG','pantalla-08.JPEG','pantalla-09.JPEG','pantalla-10.JPEG','pantalla-11.JPEG','pantalla-12.JPEG','pantalla-13.PNG','pantalla-14.JPEG','pantalla-15.JPEG','pantalla-16.JPEG','pantalla-17.PNG','pantalla-18.JPEG','pantalla-19.JPEG','pantalla-20.PNG'];
 
 const pool = ['pool-01.jpg','pool-02.jpg','pool-03.jpg','pool-04.jpg','pool-05.jpg','pool-06.jpg'];
 
-const sonido = ['sonido-01.png','sonido-02.jpeg','sonido-03.jpeg','sonido-04.jpeg','sonido-05.png','sonido-06.png','sonido-07.png','sonido-08.png'];
+const sonido = ['sonido-01.PNG','sonido-02.JPEG','sonido-03.JPEG','sonido-04.JPEG','sonido-05.PNG','sonido-06.PNG','sonido-07.PNG','sonido-08.PNG'];
 
-const tarima = ['tarima-01.png','tarima-02.png','tarima-03.png','tarima-04.png'];
+const tarima = ['tarima-01.PNG','tarima-02.PNG','tarima-03.PNG','tarima-04.PNG'];
 
-const techo = ['techo-01.png','techo-02.png','techo-03.png','techo-04.png','techo-05.png','techo-06.png','techo-07.png','techo-08.jpeg'];
+const techo = ['techo-01.PNG','techo-02.PNG','techo-03.PNG','techo-04.PNG','techo-05.PNG','techo-06.PNG','techo-07.PNG','techo-08.JPEG'];
 
 const boda = ['bodas-01.jpeg','bodas-02.jpeg','bodas-03.png','bodas-04.png','bodas-05.jpeg','bodas-06.jpeg','bodas-07.jpeg','bodas-08.jpeg','bodas-09.jpeg','bodas-10.jpeg','bodas-11.jpeg'];
+
+const clases01 = ['sliderOne','sliderTwo','sliderThree','slidercuarto','sliderquinto','slidersexto','sliderseptimo','slideroctavo'];
+
 
 module.exports={
 	conciertos,
@@ -25,5 +28,6 @@ module.exports={
 	sonido,
 	tarima,
 	techo,
-	boda
+	boda,
+	clases01
 };
