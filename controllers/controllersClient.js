3b683de51331b05ba3efc4d5d9fed00229af1bdc
RandyGraterol <soapdelinger@gmail.com>
2024-09-17 21:30:03 +0200

    Update controllersClient.js
    updateV2

--- controllers/controllersClient.js
+++ controllers/controllersClient.js
@@ -1,7 +1,7 @@
-const {conciertos,estructura,iluminacion,pantallaLed,pool,sonido,tarima,techo,boda} = require('../utils/memoria.js');
+const {conciertos,estructura,iluminacion,pantallaLed,pool,sonido,tarima,techo,boda,clases01} = require('../utils/memoria.js');
 
 const index = (req,res)=>{
-res.render('index');
+res.render('index',{conciertos,estructura,iluminacion,pantallaLed,pool,sonido,tarima,techo,boda,clases01});
 }
 
 const sonidoO = (req,res)=>{
